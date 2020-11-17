3f11545ebc71c61ad20b632502c7120e71630d02
SophieBRU <68539827+SophieBRU@users.noreply.github.com>
2020-11-17 12:31:55 +0100

    Delete Select.sql

--- Select.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-	CONCAT (StudentLastName, ' ', StudentFirstName) AS 'NOM Prénom',
-	AVG(ExamNote) AS 'Moyenne'
-FROM Exam
-INNER JOIN Student ON  StudentId = FK_StudentId
-GROUP BY StudentId, StudentLastName, StudentFirstName;
